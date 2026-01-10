9332bb84c744f26bf6ff1472d6e3555c94f33d66
jopoll87 <poll.jonathan87@gmail.com>
2026-01-10 16:41:15 -0700

    testing new presidency look

--- src/components/GeneralInfo.tsx
+++ src/components/GeneralInfo.tsx
@@ -1,17 +1,23 @@
+import { Table } from 'semantic-ui-react';
+import { BISHOPRIC, PresidencyType } from '../data/AdultData';
 
 const GeneralInfo = () => {
-  
   return (
     <div className='info'>
       <h2>1st Ward General Info</h2>
-      <p><span>Bishopric: </span></p>
-            <ul>
-              <li><span>Bishop: </span>Jared Miller</li>
-              <li><span>1st Counselor: </span>Jason Poll</li>
-              <li><span>2nd Counselor: </span>Abraham McKay</li>
-              <li><span>Executive Secretary: </span>Brodie Nielson</li>
-              <li><span>Ward Clerk: </span>Matt Gustafson</li>
-            </ul>
+      <h3>Bishopric</h3>
+      <div>
+        <Table basic='very' celled collapsing>
+          <Table.Body>
+            {BISHOPRIC.map((member: PresidencyType) => (
+              <Table.Row key={member.id}>
+                <Table.Cell><strong>{member.calling}:</strong> {member.name}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+      <hr/>
       <p>
         The South Weber First Ward Meets on Sundays from{' '}
         <span>8:30am to 10:30am.</span>
--- src/components/activities/Primary.tsx
+++ src/components/activities/Primary.tsx
@@ -1,18 +1,60 @@
+import { Table } from 'semantic-ui-react';
+import {
+  PRIMARYCLASSES,
+  PrimaryClassType,
+  PRIMARYPRESIDENCY,
+  PresidencyType,
+} from '../../data/PrimaryData';
 
 const Primary = () => {
   return (
     <div className='activities'>
       <h2>Primary</h2>
-      <p><span>Presidency: </span></p>
-      <ul>
-        <li><span>President: </span>Karson Hall</li>
-        <li><span>1st Counselor: </span>Ellie Jeppsen</li>
-        <li><span>2nd Counselor: </span>Gina Colby</li>
-        <li><span>Secretary: </span>Jodee Baltazar</li>
-      </ul>
-      <p><span>Upcoming Activities: </span>TBD</p>
+      <p>
+        <span>Presidency: </span>
+      </p>
+      <div>
+        <Table basic='very' celled collapsing>
+          <Table.Body>
+            {PRIMARYPRESIDENCY.map((member: PresidencyType) => (
+              <Table.Row key={member.id}>
+                <Table.Cell>
+                  <strong>{member.calling}:</strong> {member.name}
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
+      <hr />
+      <p>
+        <span>Upcoming Activities: </span>TBD
+      </p>
+      <p>Singing Time: 9:40-10:00am</p>
+      <p>Class Time: 10:00-10:30am</p>
+      <h3>Primary Classrooms</h3>
+      <div className='general-conference'>
+        <Table celled basic='very' style={{ marginTop: '30px' }}>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Course</Table.HeaderCell>
+              <Table.HeaderCell>Teachers</Table.HeaderCell>
+              <Table.HeaderCell>Classroom</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {PRIMARYCLASSES.map((course: PrimaryClassType) => (
+              <Table.Row key={course.id}>
+                <Table.Cell>{course.course}</Table.Cell>
+                <Table.Cell>{course.teachers}</Table.Cell>
+                <Table.Cell>{course.classroom}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Primary
+export default Primary;
